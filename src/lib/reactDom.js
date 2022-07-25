2893944224591f668a254ca3a798db31b60ca769
ginlink <ginlink1998@gmail.com>
2022-07-25 08:38:46 +0800

    feat: add 02.concurrent mode

--- src/lib/reactDom.js
+++ src/lib/reactDom.js
@@ -1,4 +1,70 @@
-function render(element, container) {
+let nextUnitWork;
+
+function perforUnitOfWork(fiber) {
+  // 创建DOM
+  // 为当前fiber创建子fiber
+  // 返回下一个执行单元
+
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props?.children
+  let prevSibling = null
+  elements && elements.forEach((childElement, index) => {
+    const newFiber = {
+      parent: fiber,
+      props: childElement.props,
+      type: childElement.type,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+  })
+
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+
+    nextFiber = nextFiber.fiber
+  }
+}
+
+function workLoop(deadline) {
+  // 是否有下一个任务
+  // 是否有剩余时间
+  // 执行任务
+
+  let shouldYield = true
+
+  while (nextUnitWork && shouldYield) {
+    shouldYield = deadline.timeRemaining() > 1
+
+    nextUnitWork = perforUnitOfWork(nextUnitWork)
+  }
+
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function createDom(element) {
   const dom =
     element.type === 'TEXT_ELEMENT'
       ? document.createTextNode('')
@@ -9,9 +75,18 @@ function render(element, container) {
     .filter(isProperty)
     .forEach((key) => dom[key] = element.props[key]);
 
-  element.props?.children?.forEach((child) => render(child, dom))
+  return dom
+}
 
-  container.appendChild(dom)
+function render(element, container) {
+  // 构建fiber
+  nextUnitWork = {
+    dom: container,
+    props: {
+      children: [element]
+    },
+    parent: undefined,
+  }
 }
 
 const ReactDom = {
